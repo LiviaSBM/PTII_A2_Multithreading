6c1968ba89f608d8efb57c671a15ecfbcdd078b8
Livia Menezes <liviasbm@hotmail.com>
2022-10-16 16:35:04 -0400

    Kick off
    Solved issue with importing global variable without passing parameter in

--- A2.java
+++ A2.java
@@ -6,13 +6,22 @@ import java.util.List;
 import java.util.Random;
 
 public class A2 {
-    static int cheese= 10; //initial cheese quantity
+    public static int cheese= 10; //initial cheese quantity
     static int x = 5; //initial mice qty
-    static int y = 3 //Initial qty of traps
+    static int y = 3; //Initial qty of traps
     static int index = 1; //initial index
-    static int lost_cheese = 0 //initial qty of lost cheese
+    static int lost_cheese = 0; //initial qty of lost cheese
+    static List mice_list = new ArrayList(); //List of mouses
+    static List dead_mice_list = new ArrayList(); //List of dead mouses
 
     public static void main(String[] args) {
-        
+        for (int a=0; a<x; a++){ //fulfilling the list of mouse
+            mice_list.add(a+1);
+        }
+
+        MousesThread mousesthread = new MousesThread();
+        Thread myThread = new Thread(mousesthread);
+
+        myThread.start();
     }
 }
--- MousesThread.java
+++ MousesThread.java
@@ -1,6 +1,20 @@
-public class MousesThread extends A2{
+public class MousesThread extends A2 implements Runnable{
     
+    @Override
     public void run(){
+
+        for(int i=0; i<5; i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+
+    System.out.println(cheese);
+
         
+
+
     }
 }
